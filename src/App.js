5b8bc823ce5dc18da981d932a39c9b0b1e6d2249
nataly_p_v@ukr.net <nataly_p_v@ukr.net>
2018-02-25 18:51:47 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import Table from "./components/Table";
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            score: 0,
+        };
+    }
+
+    componentDidMount(){
+
+    }
+
+    render() {
+        return (
+            < div className="App">
+            asd
+        </div>
+    );
+    }
+}
+
+export default App;
