5bfcf3f3b6fe2c844e9f98613e7a3d868476f899
Natalia <nataly_p_v@mail.ru>
2018-03-20 12:34:06 +0200

    Update DatePickerComponent.js

--- src/components/DatePickerComponent.js
+++ src/components/DatePickerComponent.js
@@ -4,19 +4,27 @@ import '../App.css';
 
 
 class DatePickerComponent extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-
+            time:props.dataTime
         };
+        this.handleChange = this.handleChange.bind(this)
     }
     componentDidMount() {
+    }
+    handleChange(e){
+        console.log(this.state.time)
+        this.state.time =  e.target.value;
+        this.setState({
+            time: e.target.value
+        });
     }
 
     render() {
         return (
             <div>
-                <input id="time" type="time"  value={this.props.dataTime} onChange={(e) => this.props.handleChange(e)}/>
+                <input id="time" type="time"  value={this.state.time} onChange={(e) => this.handleChange(e)}/>
             </div>
         );
     }
